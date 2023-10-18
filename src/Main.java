5026d8e8b2321bf5ad95c4849e7e367ff0c739d2
Hoseong Ahn <hoseong8115@naver.com>
2023-10-18 19:34:50 +0900

    fxcking overriding

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
-public class Main {
+class Super {
+    public void test() {
+        System.out.println("Super");
+    }
+
+    public void function() {
+        System.out.println("Not overrided");
+    }
+}
+
+class Sub extends Super {
+    public void test() {
+        System.out.println("Sub");
+    }
+}
+
+class Sub2 extends Sub {
+}
+
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Super s = new Sub();
+        s.test();
+        s.function();
+
+        Super s1 = new Sub();
+        s1.test();
+        s1.function();
+
+        Sub2 s2 = new Sub2();
+        s2.test();
+        s2.function();
     }
 }
